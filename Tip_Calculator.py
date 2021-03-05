857570bf5fd838f0957c282d0c465a41bfed87d9
Robert Sancez <robsanchez124@gmail.com>
2021-03-04 20:02:10 -0500

    Changed file

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -4,7 +4,7 @@ dinner = float(input("How much did your dinner cost:\n "))
 tip = int(input("Enter you tip %:\n "))
 tax = .08 
 party_size = int(input('Number of friends in party:\n '))
-print(type(party_size))
+print(party_size)
 
 # Calculations
 tax_amount = dinner * tax
@@ -12,8 +12,9 @@ tip_amount = dinner * tip/100
 total = dinner + tip_amount + tax_amount
 total_per_person = total/party_size
 
-print(f"\nYour dinner was ${dinner:.2f} and your tip was ${tip_amount:.2f}.\n")
-print(f"\nYour personal total with tax was ${total_per_person:.2f}.\n")
+print(f"\nThe dinner was ${dinner:.2f}.\n")
+print(f"\nEach one of you bozo's should pay ${total_per_person:.2f}.\n")
+print(f"The tip was ${tip_amount:.2f}.\n")
 print(f"The total cost of your dinner was ${total:.2f}.\n")
 
 
